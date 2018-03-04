d93752aaaaa849fd21a4f018ffeb3aeed0d9f185
danchal <danchal@users.noreply.github.com>
2018-03-04 02:28:30 +0100

    wip

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as sudo/root"
+  exit
+fi
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+SYSTEMD_UNIT="dnsmasq-log-anonomiser"
+
+# do not clobber an existing config file
+cp -n ${SYSTEMD_UNIT}.conf /etc/
+
+# create systemd service
+cp ${SYSTEMD_UNIT}.service /etc/systemd/system/
+cp ${SYSTEMD_UNIT} /usr/local/sbin/
+
+systemctl daemon-reload
+systemctl enable ${SYSTEMD_UNIT}.service
